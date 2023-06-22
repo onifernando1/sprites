7c4c82ad0f74d40ee7f715db549286e30b2cb3fd
onifernando1 <onifernando1@gmail.com>
2023-06-22 21:32:36 +0100

    skeleton code and canvas

---
+++ script.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementById("canvas1"); // reference HTML canvas element
+const c = canvas.getContext("2d"); // Get 2d drawing methods
+const CANVAS_WIDTH = (canvas.width = 600); // capitals for global variable
+const CANVAS_HEIGHT = (canvas.height = 600);
